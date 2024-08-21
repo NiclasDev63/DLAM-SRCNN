bcfbc244e506f9b29c2f573d21ceddf05ece5c55
SinusLinus <linus_tu@yahoo.com>
2024-08-21 10:52:53 +0200

    from relu to gelu

--- models.py
+++ models.py
@@ -7,10 +7,10 @@ class SRCNN(nn.Module):
         self.conv1 = nn.Conv2d(num_channels, 64, kernel_size=9, padding=9 // 2)
         self.conv2 = nn.Conv2d(64, 32, kernel_size=5, padding=5 // 2)
         self.conv3 = nn.Conv2d(32, num_channels, kernel_size=5, padding=5 // 2)
-        self.relu = nn.ReLU()
+        self.gelu = nn.GELU()
 
     def forward(self, x):
-        x = self.relu(self.conv1(x))
-        x = self.relu(self.conv2(x))
+        x = self.gelu(self.conv1(x))
+        x = self.gelu(self.conv2(x))
         x = self.conv3(x)
         return x
