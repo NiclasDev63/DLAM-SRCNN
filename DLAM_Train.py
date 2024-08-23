a59456b411c1c587dd4d3eb007e4cbcf9f18825f
NiclasDev63 <niclas.gregor20@gmail.com>
2024-08-23 22:18:35 +0200

    trained models with different settings

---
+++ DLAM_Train.py
@@ -0,0 +1,158 @@
+import csv
+
+import torch
+import torch._dynamo
+import torch.nn as nn
+from torch.utils.data.dataloader import DataLoader
+from tqdm import tqdm
+
+from datasets import EvalDataset, TrainDataset
+from Gloss import SSIM, GradientVariance
+from models import SRCNN
+
+# Suppress missing triton errors while compiling
+torch._dynamo.config.suppress_errors = True
+
+
+def calc_psnr(MSE):
+    return 10 * torch.log10(1 / MSE)
+
+
+def saveEpochStats(epoch, train_loss, eval_loss, psnr, scale, train_name):
+    csv_file = open(
+        f"./DLAM_weights/{train_name}_epoch_stats_srcnn_x{scale}.csv", mode="a"
+    )
+    writer = csv.writer(csv_file)
+    writer.writerow([epoch + 1, train_loss, eval_loss, psnr])
+    csv_file.close()
+
+
+if __name__ == "__main__":
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = SRCNN()
+    # model.load_state_dict(torch.load('./weights/srcnn_x4.pth', map_location=device))
+    # model = torch.compile(model)
+    print("Using device: ", device)
+    model.to(device)
+
+    # G-Loss params
+    patch_size = 8
+    gradloss_weight = 0.01
+
+    # Train params
+    epochs = 150
+    batch_size = 16
+    num_workers = 8
+    lr = 1e-4
+    scale = 3
+
+    # base -> from github repo (just to see if we can reproduce the results) -- PSNR: 33.18
+    # g_loss_mse -> with gradient variance loss -- PSNR: 33.10, 32.90
+    # g_loss_ssim -> with gradient variance loss and ssim -- PSNR:
+    # adam_w -> with adam_w optimizer -- PSNR: 33.23
+    # gelu -> with gelu activation -- PSNR: 32.99
+    # TODO: change
+    train_name = "g_loss_mse"
+
+    train_file = f"./91_Image_train/91-image_x{scale}.h5"
+    eval_file = f"./Set_5_eval/Set5_x{scale}.h5"
+
+    mse_criterion = nn.MSELoss()
+    grad_criterion = GradientVariance(patch_size=patch_size).to(device)
+    ssim_criterion = SSIM().to(device)
+    optimizer = torch.optim.Adam(
+        [
+            {"params": model.conv1.parameters()},
+            {"params": model.conv2.parameters()},
+            {"params": model.conv3.parameters(), "lr": lr * 0.1},
+        ],
+        lr=lr,
+    )
+
+    train_dataset = TrainDataset(train_file)
+    train_dataloader = DataLoader(
+        dataset=train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=True,
+        drop_last=True,
+    )
+    eval_dataset = EvalDataset(eval_file)
+    eval_dataloader = DataLoader(dataset=eval_dataset, batch_size=1)
+
+    best_psnr = float("-inf")
+
+    for epoch in range(epochs):
+        train_loss = 0
+        eval_loss = 0
+        psnr = 0
+        model.train()
+        for data in tqdm(train_dataloader):
+            inputs, labels = data
+
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            preds = model(inputs)
+
+            # loss_ssim = ssim_criterion(preds, labels)
+
+            loss_mse = mse_criterion(preds, labels)
+
+            loss_grad = gradloss_weight * grad_criterion(preds, labels)
+
+            loss = loss_mse + loss_grad
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.sum().item()
+        train_loss /= len(train_dataloader)
+
+        model.eval()
+        with torch.no_grad():
+            for data in eval_dataloader:
+                inputs, labels = data
+
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                preds = model(inputs)
+
+                loss_mse = mse_criterion(preds, labels)
+
+                # loss_ssim = ssim_criterion(preds, labels)
+
+                loss_grad = gradloss_weight * grad_criterion(preds, labels)
+
+                loss = loss_mse + loss_grad
+
+                eval_loss += loss.sum().item()
+
+                psnr += calc_psnr(loss_mse).sum().item()
+
+        eval_loss /= len(eval_dataloader)
+        psnr /= len(eval_dataloader)
+
+        if psnr > best_psnr:
+            print(f"New Best... saving model with PSNR: {psnr}")
+            best_psnr = psnr
+            torch.save(
+                {
+                    "epoch": epoch + 1,
+                    "model_state_dict": model.state_dict(),
+                    "optimizer_state_dict": optimizer.state_dict(),
+                    "train_loss": train_loss,
+                    "eval_loss": eval_loss,
+                    "psnr": psnr,
+                },
+                f"./DLAM_weights/{train_name}_srcnn_x{scale}.pth",
+            )
+
+        saveEpochStats(epoch, train_loss, eval_loss, psnr, scale, train_name)
+        print(
+            f"Epoch {epoch+1}/{epochs} - Train Loss: {train_loss} - Eval Loss: {eval_loss} - PSNR: {psnr}"
+        )
--- test.py
+++ test.py
@@ -4,46 +4,59 @@ import numpy as np
 import PIL.Image as pil_image
 import torch
 import torch.backends.cudnn as cudnn
+
 from models import SRCNN
 from utils import calc_psnr, convert_rgb_to_ycbcr, convert_ycbcr_to_rgb
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights-file', type=str, required=True)
-    parser.add_argument('--image-file', type=str, required=True)
-    parser.add_argument('--scale-down', type=bool, default=False)
-    parser.add_argument('--scale', type=int, default=3)
+    parser.add_argument("--weights-file", type=str, required=True)
+    parser.add_argument("--image-file", type=str, required=True)
+    parser.add_argument("--scale-down", type=bool, default=False)
+    parser.add_argument("--scale", type=int, default=3)
+    parser.add_argument("--DLAM-weights", type=bool, default=False)
     args = parser.parse_args()
 
     cudnn.benchmark = True
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     model = SRCNN().to(device)
 
     state_dict = model.state_dict()
-    for n, p in torch.load(args.weights_file, map_location=lambda storage, loc: storage).items():
-        if n in state_dict.keys():
-            state_dict[n].copy_(p)
-        else:
-            raise KeyError(n)
+    if args.DLAM_weights:
+        model.load_state_dict(
+            torch.load(args.weights_file, map_location=device, weights_only=True)[
+                "model_state_dict"
+            ]
+        )
+    else:
+        model.load_state_dict(
+            torch.load(args.weights_file, map_location=device, weights_only=True)
+        )
 
     model.eval()
 
-    image = pil_image.open(args.image_file).convert('RGB')
+    image = pil_image.open(args.image_file).convert("RGB")
 
     image_width = (image.width // args.scale) * args.scale
     image_height = (image.height // args.scale) * args.scale
     image = image.resize((image_width, image_height), resample=pil_image.BICUBIC)
     if args.scale_down:
-        image = image.resize((image.width // args.scale, image.height // args.scale), resample=pil_image.BICUBIC)
-        image = image.resize((image.width * args.scale, image.height * args.scale), resample=pil_image.BICUBIC)
-        image.save(args.image_file.replace('.', '_bicubic_x{}.'.format(args.scale)))
+        image = image.resize(
+            (image.width // args.scale, image.height // args.scale),
+            resample=pil_image.BICUBIC,
+        )
+        image = image.resize(
+            (image.width * args.scale, image.height * args.scale),
+            resample=pil_image.BICUBIC,
+        )
+        image.save(args.image_file.replace(".", "_bicubic_x{}.".format(args.scale)))
 
     image = np.array(image).astype(np.float32)
     ycbcr = convert_rgb_to_ycbcr(image)
 
     y = ycbcr[..., 0]
-    y /= 255.
+    y /= 255.0
     y = torch.from_numpy(y).to(device)
     y = y.unsqueeze(0).unsqueeze(0)
 
@@ -51,11 +64,11 @@ if __name__ == '__main__':
         preds = model(y).clamp(0.0, 1.0)
 
     psnr = calc_psnr(y, preds)
-    print('PSNR: {:.2f}'.format(psnr))
+    print("PSNR: {:.2f}".format(psnr))
 
     preds = preds.mul(255.0).cpu().numpy().squeeze(0).squeeze(0)
 
     output = np.array([preds, ycbcr[..., 1], ycbcr[..., 2]]).transpose([1, 2, 0])
     output = np.clip(convert_ycbcr_to_rgb(output), 0.0, 255.0).astype(np.uint8)
     output = pil_image.fromarray(output)
-    output.save(args.image_file.replace('.', '_srcnn_x{}.'.format(args.scale)))
+    output.save(args.image_file.replace(".", "_srcnn_x{}.".format(args.scale)))
