4b9aa166485ad97c511f96a55c56dee403b2bc17
dhia-benaicha <dhiabenaicha00@gmail.com>
2024-09-15 13:06:30 +0200

    residuals

--- models.py
+++ models.py
@@ -8,12 +8,18 @@ class SRCNN(nn.Module):
         self.conv2 = nn.Conv2d(64, 32, kernel_size=5, padding=5 // 2)
         self.conv3 = nn.Conv2d(32, num_channels, kernel_size=5, padding=5 // 2)
         self.relu = nn.ReLU()
-        self.gelu = nn.GELU()
+
+        self.residual_conv1 = nn.Conv2d(num_channels, 64, kernel_size=1)
+        self.residual_conv2 = nn.Conv2d(64, 32, kernel_size=1)
+        self.residual_conv3 = nn.Conv2d(32, num_channels, kernel_size=1)
 
     def forward(self, x):
-        x = self.relu(self.conv1(x))
-        x = self.relu(self.conv2(x))
-        # x = self.gelu(self.conv1(x))
-        # x = self.gelu(self.conv2(x))
-        x = self.conv3(x)
+        residual = self.residual_conv1(x)
+        x = self.relu(self.conv1(x) + residual)
+
+        residual = self.residual_conv2(x)
+        x = self.relu(self.conv2(x) + residual)
+
+        residual = self.residual_conv3(x)
+        x = self.conv3(x) + residual
         return x
